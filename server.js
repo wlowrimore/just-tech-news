d406f54fc3a89c74dd5093ccc38c9ca942f26211
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-03-09 11:53:53 -0600

    adds comment routes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
-// turn on connecction to db and server
-sequelize.sync({ force: false }).then(() => {
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
